3330a263f3a559bb9f14793b10cb6e1d2437c74b
James Weng <wengj9@gmail.com>
2024-05-10 21:26:37 -0700

    Don't minify output

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   clean: true,
-  minify: true,
+  sourcemap: true,
   dts: true,
   format: ["cjs", "esm"],
   tsconfig: "tsconfig.build.json",
